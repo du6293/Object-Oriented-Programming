85aef20ff7e70b113c1d6f5d08a85ef48fbe5a41
seon deok Kim <76850241+du6293@users.noreply.github.com>
2025-03-01 13:55:36 +0900

    Create 6.9.cpp

---
+++ Chapter6/6.9.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+/* 포인터 연산과 배열 인덱싱 */
+int main()
+{
+	// 정적 배열도 포인터이다.
+	char name[] = "jackjack";
+	char* ptr = name;
+
+	const int n_name = sizeof(name) / sizeof(name[0]);
+
+	for (int i = 0; i < n_name; ++i)
+	{
+		// null 까지 모두 출력됨
+		//cout << *(name + i) ;
+		cout << *(ptr + i); // 윗줄과 결과 동일
+	}
+	cout << endl;
+
+	
+	while (*(ptr) != '\0')
+	{
+		
+		cout << *(ptr) << endl;
+		ptr++;
+
+	}
+
+	return 0;
+}
