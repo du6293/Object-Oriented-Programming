995e8a0c56b79634676f71de15901c6bca2a8166
seon deok Kim <76850241+du6293@users.noreply.github.com>
2025-03-01 14:13:03 +0900

    Update 6_10_1.cpp

--- Chapter6/6_10/6_10_1.cpp
+++ Chapter6/6_10/6_10_1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-/* C언어 스타일의 문자열 심볼릭 상수 */
+/* C언어 스타일의 문자열 심볼릭 상수 
+	c++ compiler는 문자열 리터럴이 중복되면 메모리르 절약하기 위해 하나의 리터럴만 저장하고 공유한다.
+*/
 using namespace std;
 
 // const 포인터변수 는 함수의 리턴형으로 사용 가능하다. 
@@ -18,7 +20,7 @@ int main()
 	const char* name2 = getName();
 
 	// 컴파일러가 두 리터럴 변수의 내용이 같으므로 같은 메모리 주소를 사용하도록 함
-	// getName()이 호출될 때마다 동일한 리터럴을 가리키는 포인터를 반
+	// getName()이 호출될 때마다 동일한 리터럴(=동일한 memory address)을 가리키는 포인터를 반환
 	cout << (uintptr_t)name << endl; // uintptr_t: 포인터값을 정수형 주소값으로 변환해서 출력
 	cout << (uintptr_t)name2 << endl;
 
